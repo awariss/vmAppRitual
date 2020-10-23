d0d6627888031d3f5c7ae370aa82c0be50eb69ff
David Lang <david.lang398@gmail.com>
2020-10-23 14:31:15 +0200

    serving app (#1)

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 80
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log("Server is running on port: " + port)
 })
